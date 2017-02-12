ef17757fe617dc924b7249c897242626db4a4d28
Tanner Summers <tgsummers@cpp.edu>
2017-02-11 17:37:02 -0800

    - Able to connect to android app - Able to parse JSON to 2d array

--- src/main.py
+++ src/main.py
@@ -5,13 +5,12 @@ import struct
 import numpy as np
 import json
 
-HOST = '127.0.0.1'  # Symbolic name, meaning all available interfaces
-PORT = 8089  # Arbitrary non-privileged port
+HOST = ''
+PORT = 8089
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print ('Socket created')
 
-# Bind socket to local host and port
 try:
     s.bind((HOST, PORT))
 except socket.error as msg:
@@ -48,71 +47,6 @@ json_str = json_str.strip('\n')
 json_arr = np.array(json.loads(json_str))
 
 print("DEBUG: " + str(json_arr))
-
-
-
-# Receiving from client
-# data = conn.recv(10000)
-
-
-# values = bytearray(json_str, 'utf-8')
-# values = bytes(json_str, 'utf-8')
-
-# print("DEBUG: " + str(values))
-
-
-# json_str2 = np.array(json.loads(json_str))
-
-# print(json_str2)
-
-
-# while True:
-#     new_data = conn.recv(1)
-#     if new_data:
-#         json_str += str(new_data)
-#     else:
-#         json_str += str(new_data[:])
-#         print ("no more data from" )
-#         break
-#
-# print("DATA: "  +str(json_str[:-1]))
-#
-
-
-# print ("DEBUG -----" + str(data))
-
-# unpacked_data = struct.unpack("s",values)
-
-# unpacked_data = struct.unpack("=s",data)
-
-
-
-
-# print("DEBUG: " + json.loads(data))
-
-
-
-# print (json_str)
-
-
-# unpacked_data = struct.unpack("",data)
-
-#
-# unpacked_data = struct.unpack("@s", data)
-# unpacked_data2 = struct.unpack("=s", data)
-# unpacked_data3= struct.unpack(">s", data)
-#
-#  # intel is little endian, java vm is big endian
-# unpacked_data4  = struct.unpack("<s", data)
-# unpacked_data5  = struct.unpack("!s", data)
-# unpacked_data6  = struct.unpack("s", data)
-
-# print("DATA3: " + str(unpacked_data))
-# print("DATA5: " + str(unpacked_data2))
-# print("DATA3: " + str(unpacked_data3))
-# print("DATA5: " + str(unpacked_data4))
-# print("DATA5: " + str(unpacked_data5))
-# print("DATA5: " + str(unpacked_data6))
 
 
 
